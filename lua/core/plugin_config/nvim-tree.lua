3452b638a732fcae7bb3fdefeae3660d953266d4
andlu434 <andlu434@student.liu.se>
2023-11-25 20:55:16 +0100

    always start up nvim tree

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -4,3 +4,12 @@ vim.g.loaded_netrwPlugin = 1
 require("nvim-tree").setup()
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
+
+local function open_nvim_tree()
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
