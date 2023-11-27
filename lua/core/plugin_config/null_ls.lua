be6d739e0a77dedb1b0bf8ee0d73b8db78598cc9
andlu434 <andlu434@student.liu.se>
2023-11-27 08:30:48 +0100

    autoformat on save

--- lua/core/plugin_config/null_ls.lua
+++ lua/core/plugin_config/null_ls.lua
@@ -1,3 +1,4 @@
+local au_group = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require("null-ls")
 
 -- register any number of sources simultaneously
@@ -5,4 +6,21 @@ local sources = {
     null_ls.builtins.formatting.clang_format,
 }
 
-null_ls.setup({ sources = sources })
+null_ls.setup({
+  sources = sources,
+  on_attach = function (client, bufnr)
+   if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = au_group,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = au_group,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end
+      })
+   end
+  end
+})
