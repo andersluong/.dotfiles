a58a122eaa9211ee5a7d8997ef4dca7bd200d0d5
andlu434 <andlu434@student.liu.se>
2023-11-28 11:32:23 +0100

    move lines

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -12,14 +12,20 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
+
 --Line numbers
 vim.wo.number = true
 
 vim.cmd('setlocal signcolumn=yes')
 
+-- Copy to clipboard
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.keymap.set('n','y','"+y')
 vim.keymap.set('n','yy','"+yy')
 vim.keymap.set('n','Y','"+Y')
 vim.keymap.set('x','y','"+y')
 vim.keymap.set('x','Y','"+Y')
+
+-- Move lines
+vim.keymap.set('n','<C-j>',':m .+1<CR>==')
+vim.keymap.set('n','<C-k>',':m .-2<CR>==')
