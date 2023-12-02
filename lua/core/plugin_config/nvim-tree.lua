08d3394ac3d9c2610345e903d42fdce2c71f32fd
Anders Luong <andersluong@gmail.com>
2023-12-02 10:40:04 +0100

    dont startup tree if file open

--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -5,7 +5,12 @@ require("nvim-tree").setup()
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
 
-local function open_nvim_tree()
+local function open_nvim_tree(data)
+
+  -- only files
+  if vim.fn.filereadable(data.file) == 1 then
+    return
+  end
 
   -- open the tree
   require("nvim-tree.api").tree.open()
