87c89e2f487141a474173eed957896085cfbc6b2
Anders Luong <andersluong@gmail.com>
2024-03-02 22:00:00 +0100

    updated kitty config

--- nvim/.config/nvim/lua/core/resources/tools.lua
+++ nvim/.config/nvim/lua/core/resources/tools.lua
@@ -57,9 +57,9 @@ return {
     vim.o.timeoutlen = 300
   end,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+      window = {
+        border = "single",
+      }
   }
 }
 }
